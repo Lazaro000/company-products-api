03550c1b05e4208a3adbac94bb8e5a6b2a3ee01a
Lázaro <lazaroyaesta@gmail.com>
2022-07-28 23:29:55 +0200

    🥅 Catch Errors

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,59 +6,67 @@ import { RoleSchema } from 'src/models/Role.js';
 dotenvConfig();
 
 export const signUp = async (req, res) => {
-  const { username, email, password, roles } = req.body;
+  try {
+    const { username, email, password, roles } = req.body;
 
-  const newUser = new UserSchema({
-    username,
-    email,
-    password: await UserSchema.encryptPassword(password),
-  });
+    const newUser = new UserSchema({
+      username,
+      email,
+      password: await UserSchema.encryptPassword(password),
+    });
 
-  if (roles) {
-    const foundRoles = await RoleSchema.find({ name: { $in: roles } });
+    if (roles) {
+      const foundRoles = await RoleSchema.find({ name: { $in: roles } });
 
-    newUser.roles = foundRoles.map((role) => role._id);
-  } else {
-    const role = await RoleSchema.findOne({ name: 'user' });
+      newUser.roles = foundRoles.map((role) => role._id);
+    } else {
+      const role = await RoleSchema.findOne({ name: 'user' });
 
-    newUser.roles = [role._id];
+      newUser.roles = [role._id];
+    }
+
+    const savedUser = await newUser.save();
+
+    const token = jwt.sign({ id: savedUser._id }, process.env.JWT_PRIVATE_KEY, {
+      expiresIn: 86400, // 24 hours
+    });
+
+    return res.status(200).json({ token });
+  } catch (err) {
+    return res.status(500).json(err.message);
   }
-
-  const savedUser = await newUser.save();
-
-  const token = jwt.sign({ id: savedUser._id }, process.env.JWT_PRIVATE_KEY, {
-    expiresIn: 86400, // 24 hours
-  });
-
-  return res.status(200).json({ token });
 };
 
 export const signIn = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const userFound = await UserSchema.findOne({
-    email,
-  }).populate('roles');
+    const userFound = await UserSchema.findOne({
+      email,
+    }).populate('roles');
 
-  if (!userFound) return res.status(400).json({ message: 'User not found' });
+    if (!userFound) return res.status(400).json({ message: 'User not found' });
 
-  const matchPassword = await UserSchema.comparePassword(
-    password,
-    userFound.password
-  );
+    const matchPassword = await UserSchema.comparePassword(
+      password,
+      userFound.password
+    );
 
-  if (!matchPassword)
-    return res.status(401).json({ token: null, message: 'Invalid password' });
+    if (!matchPassword)
+      return res.status(401).json({ token: null, message: 'Invalid password' });
 
-  const token = jwt.sign(
-    {
-      id: userFound._id,
-    },
-    process.env.JWT_PRIVATE_KEY,
-    {
-      expiresIn: 86400,
-    }
-  );
+    const token = jwt.sign(
+      {
+        id: userFound._id,
+      },
+      process.env.JWT_PRIVATE_KEY,
+      {
+        expiresIn: 86400,
+      }
+    );
 
-  return res.json({ token });
+    return res.json({ token });
+  } catch (err) {
+    return res.status(500).json(err.message);
+  }
 };
