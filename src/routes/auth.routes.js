c7c3bd11df4205e4d9b49ea7127fbc0c004de853
Lázaro <lazaroyaesta@gmail.com>
2022-07-28 23:53:51 +0200

    🚸 Add Headers

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,6 +7,14 @@ import {
 
 const router = Router();
 
+router.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Headers',
+    'x-access-token, Origin, Content-Type, Accept'
+  );
+  next();
+});
+
 router.post('/signup', [checkExistingUser, checkExistingRole], signUp);
 router.post('/signin', signIn);
 
